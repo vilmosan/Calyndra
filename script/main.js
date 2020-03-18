14e4e520aead162bdab2dff96f5d3c277899af53
vilmosan <vvebdevelopment@gmail.com>
2020-03-18 20:55:52 +0100

    Added base structure for Calyndra.
    Tried my luck in simulating keyboard presses with button clicks. It works!

---
+++ script/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    /**
+     * Keypress osztályban lévő gombok lenyomása után
+     * kiszedi a hozzájuk társított ASCII-kódot amit megadtunk
+     * HTML-ben a data-code paramétereként, majd hozzáfúzi
+     * az input id-vel ellátott input fieldhez az adott karaktert.
+     */
+    $('.keypress').click(function() {
+        var code = $(this).data('code');
+        $('#input').val(function(i, val) {
+            return val + String.fromCharCode(code);                
+     })
+    });
+});
