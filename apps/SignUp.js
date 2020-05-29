364365d058054ae56c995a656db2a375373c687b
vilmosan <vvebdevelopment@gmail.com>
2020-05-29 19:06:32 +0200

    Just some minor fixes for Sign-up frontend.

--- apps/SignUp.js
+++ apps/SignUp.js
@@ -15,7 +15,7 @@ export class signUp extends Application {
         this.canvas.append(signup_div);
         signup_div.className = "container";
         signup_div.id = "signupdiv";
-        signup_div.style="padding: 16px; margin-left:10%"
+        signup_div.style="padding: 16px; margin-left:10%";
 
         const h1element = document.createElement('h1');
         signup_div.append(h1element);
@@ -30,7 +30,7 @@ export class signUp extends Application {
         const paragraph2 = document.createElement("p");
         paragraph2.textContent = "You have to accept our terms and policy.";
         signup_div.append(paragraph2);
-        paragraph2.style = "margin-bottom:10px"
+        paragraph2.style = "margin-bottom:10px";
         
         
         //first name, last name
@@ -50,7 +50,7 @@ export class signUp extends Application {
         firstname_textbox.className =("form-control");
         firstname_textbox.placeholder="First name";
         firstname_textbox.type="text";
-        firstname_textbox.id = "firstname"
+        firstname_textbox.id = "firstname";
         firstname_div.append(firstname_textbox);
 
         const lastname_div = document.createElement("div");
@@ -61,7 +61,7 @@ export class signUp extends Application {
         lastname_textbox.className =("form-control");
         lastname_textbox.placeholder="Last name";
         lastname_textbox.type="text";
-        lastname_textbox.id="lastname"
+        lastname_textbox.id="lastname";
         lastname_div.append(lastname_textbox);
 
  //username
@@ -79,14 +79,14 @@ export class signUp extends Application {
         username_textbox.className =("form-control");
         username_textbox.placeholder="Username";
         username_textbox.type="text";
-        username_textbox.id="username"
+        username_textbox.id="username";
         username_div.append(username_textbox);
        
 //email
 
         const signupformemail_div = document.createElement("div");
-        signupformemail_div.className=("form-group")
-        signup_div.append(signupformemail_div)
+        signupformemail_div.className=("form-group");
+        signup_div.append(signupformemail_div);
 
 
         const signupform_emaillabel = document.createElement("label");
@@ -95,10 +95,10 @@ export class signUp extends Application {
 
 
         const email_textbox = document.createElement("input");
-        email_textbox.placeholder = ("Type your email adress here")
-        email_textbox.type = ("email")
-        email_textbox.className=("form-control")
-        email_textbox.id =("email")
+        email_textbox.placeholder = ("Type your email adress here");
+        email_textbox.type = ("email");
+        email_textbox.className=("form-control");
+        email_textbox.id =("email");
         signupformemail_div.append(email_textbox);
 //jelszó       
 
@@ -109,13 +109,14 @@ export class signUp extends Application {
         const signupformpassword_label = document.createElement("label"); 
         signupformpassword_div.append(signupformpassword_label);
         signupformpassword_label.textContent = ("Password:");
-        signupformpassword_label.style ="font-size: 8"
+        signupformpassword_label.style ="font-size: 8";
 
         const paragraph_password = document.createElement("p");
-        paragraph_password.textContent=" "
+        paragraph_password.textContent=" ";
         signupformpassword_div.append(paragraph_password);
 
         const password_textbox = document.createElement("input");
+        password_textbox.style = "padding: 5px;";
         password_textbox.placeholder = ("password");        
         password_textbox.classname = ("form-control");
         password_textbox.type = ("password");
@@ -124,7 +125,7 @@ export class signUp extends Application {
 
 //jelszó megerősítése
 
-        const signupformpassword2_div = document.createElement('div')
+        const signupformpassword2_div = document.createElement('div');
         signupformpassword2_div.className = "form-group";
         
         signup_div.append(signupformpassword2_div);
@@ -132,6 +133,7 @@ export class signUp extends Application {
 
 
         const password2_textbox = document.createElement("input");
+        password2_textbox.style = "padding: 5px;";
         password2_textbox.placeholder = ("confirm password");        
         password2_textbox.classname = ("form-control");
         password2_textbox.type = ("password");
@@ -145,18 +147,20 @@ export class signUp extends Application {
 
         const phonenum_label = document.createElement("label");
         phonenum_label.textContent="Phone number:";
-        phonenum_div.append(phonenum_label)
+        phonenum_div.append(phonenum_label);
 
         const phonenum_textbox = document.createElement("input");
         phonenum_div.append(phonenum_textbox);
-        phonenum_textbox.placeholder=("+36702012878")
+        phonenum_textbox.placeholder=("+36702012878");
         phonenum_textbox.type  = "text";
-        phonenum_textbox.id ="phonenum"
+        phonenum_textbox.id ="phonenum";
         phonenum_textbox.className = "form-control";
-        phonenum_div.append(phonenum_textbox)
+        phonenum_div.append(phonenum_textbox);
         
 //gender selection
 const gender_div = document.createElement("div");
+gender_div.style = "margin-top:15px;"
+gender_div.style = "margin-bottom:15px;"
 signup_div.append(gender_div);
 
 const gender_label = document.createElement("label");
@@ -169,32 +173,32 @@ gender_div.append(br);
 const male_label = document.createElement("label");
 male_label.className = ("radio-inline");
 gender_div.append(male_label);
-male_label.textContent = "Male"
+male_label.textContent = "Male";
 
 const male_input = document.createElement("input");
 male_input.type = "radio";
 male_input.name = "optradio";
 male_input.id="male";
 male_label.append(male_input);
-male_input.style = "margin-left:10px"
+male_input.style = "margin-left:10px";
 
 const female_label = document.createElement("label");
 female_label.className = ("radio-inline");
 gender_div.append(female_label);
-female_label.textContent = "Female"
-female_label.style = "margin-left:10px"
+female_label.textContent = "Female";
+female_label.style = "margin-left:10px";
 
 const female_input = document.createElement("input");
 female_input.type = "radio";
 female_input.name = "optradio";
 female_input.id="female";
 female_label.append(female_input);
-female_input.style ="margin-left:10px"
+female_input.style ="margin-left:10px";
 
 //dropdown country
 const country_label = document.createElement("label");
 signup_div.append(country_label);
-country_label.textContent = "Select your country."
+country_label.textContent = "Select your country:"
 
 const country_div = document.createElement("div");
 country_div.className = "input-group mb-3";
@@ -204,7 +208,7 @@ signup_div.append(country_div);
 const country_select = document.createElement("select");
 country_select.className = "custom-select";
 country_div.append(country_select);
-country_select.id="countyselect"
+country_select.id="countyselect";
 
 
 const br2 = document.createElement("br");
@@ -255,14 +259,14 @@ checkbox1_div.classname = "checkbox";
 checkbox_div.append(checkbox1_div);
 
 const checkbox1_label = document.createElement("label");
-checkbox1_label.textContent = "Accept terms and policys";
+checkbox1_label.textContent = "Accept our terms and policies";
 checkbox1_div.append(checkbox1_label);
 
 const checkbox1_input = document.createElement("input");
 checkbox1_input.type="checkbox";
 checkbox1_input.value = "";
 checkbox1_label.append(checkbox1_input);
-checkbox1_input.id = "checkbox1"
+checkbox1_input.id = "checkbox1";
 checkbox1_input.style = "margin-left : 10px";
 
 
@@ -271,7 +275,7 @@ checkbox2_div.className = "checkbox";
 checkbox_div.append(checkbox2_div);
 
 const checkbox2_label = document.createElement("label");
-checkbox2_label.textContent = "Subscribe our newsletter ";
+checkbox2_label.textContent = "Subscribe to our newsletter";
 checkbox2_div.append(checkbox2_label);
 
 const checkbox2_input = document.createElement("input");
@@ -286,25 +290,26 @@ checkbox2_input.style = "margin-left : 10px";
 
 //gombok
 
-        const buttons_div = document.createElement("div")
-        buttons_div.className="row"
+        const buttons_div = document.createElement("div");
+        buttons_div.className="row";
         signup_div.append(buttons_div);
-        buttons_div.style = "padding: 16px"
+        buttons_div.style = "padding: 16px";
 
-        const button_cancel = document.createElement("button");
-        button_cancel.className=" col-sm-12 col-md-6 col-lg-6 col-xl-6 btn btn-danger btn-lg";
-        button_cancel.type = "button";
-        button_cancel.textContent = "clear";
-        buttons_div.append(button_cancel);
-        button_cancel.id="clear"
 
         const button_signup = document.createElement("button");
-        button_signup.className = "col btn btn-success btn-lg"
-        button_signup.textContent = "Sing Up"
-        button_signup.type ="button"
+        button_signup.className = " col-md-5 col-lg-5 col-xl-5 btn btn-success btn-lg";
+        button_signup.textContent = "Sign up";
+        button_signup.type ="button";
         buttons_div.append(button_signup);
-        button_signup.id="signup"
+        button_signup.id="signup";
         
+        
+        const button_cancel = document.createElement("button");
+        button_cancel.className="  col-md-5 col-lg-5 col-xl-5 btn btn-danger btn-lg";
+        button_cancel.type = "button";
+        button_cancel.textContent = "Clear";
+        buttons_div.append(button_cancel);
+        button_cancel.id="clear";
         
 
       
